7a7812efba9e411f5eb1668d322e1d5f05de388a
wasanx25 <wasanx25@gmail.com>
2020-12-25 21:59:38 +0900

    yarn add -D jest ts-jest @types/jest

---
+++ src/types/Years.ts
@@ -0,0 +1,18 @@
+import { eqDate } from 'fp-ts/Date'
+import { date } from 'fp-ts'
+import { pipe } from 'fp-ts/function'
+import { map, some } from 'fp-ts/Option'
+import { fold } from 'fp-ts/boolean'
+
+eqDate.equals(new Date, new Date)
+date.now()
+
+pipe(
+  some(true),
+  map(
+    fold(
+      () => 'false',
+      () => 'true'
+    )
+  )
+)
