ea62d62ca068203c11d6b6e8e050d186730f50b6
wasanx25 <wasanx25@gmail.com>
2020-12-27 22:09:21 +0900

    Add sample test and jest-styled-components

---
+++ test/components/MonthPicker.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { MonthPicker } from '../../src/components/MonthPicker'
+import { YearAndMonth } from '../../src/types/YearAndMonth'
+import 'jest-styled-components'
+import '@testing-library/jest-dom/extend-expect'
+
+describe('MonthPicker', () => {
+  test('sample', () => {
+    const {getByRole} = render(<MonthPicker handleChange={jest.fn()} defaultValue={YearAndMonth.now()}/>)
+
+    const expected = [
+      '<thead>',
+      '<tr>',
+      '<th class="arrow">&lt;</th>',
+      '<th>2020</th>',
+      '<th class="arrow">&gt;</th>',
+      '</tr>',
+      '</thead>'
+    ].join('')
+    // expect(getByRole('month-picker-table', { hidden: true})).toMatchSnapshot()
+    expect(getByRole('month-picker-table', {hidden: true})).toContainHTML(expected)
+  })
+})
