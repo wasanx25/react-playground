2f181a2967ef41dc1069f55db7935dacbdbf9cb6
wasanx25 <wasanx25@gmail.com>
2021-02-12 01:29:52 +0900

    Update drag list

--- src/components/DraggableList.tsx
+++ src/components/DraggableList.tsx
@@ -3,6 +3,7 @@ import { StyledDraggableList } from './StyledDraggableList'
 
 const DRAGGABLE_LIST_ID = 'item-list'
 const DATA_TRANSFER_ID = 'item-id'
+const DRAG_OVER_CLASS_NAME = 'dragover'
 
 export const DraggableList = (): JSX.Element => {
   function drag (event: React.DragEvent) {
@@ -17,16 +18,16 @@ export const DraggableList = (): JSX.Element => {
     const itemList = document.getElementById(DRAGGABLE_LIST_ID)!
     itemList.insertBefore(element, event.currentTarget)
 
-    event.currentTarget.className = ''
+    event.currentTarget.classList.remove(DRAG_OVER_CLASS_NAME)
   }
 
   function dragLeave (event: React.DragEvent) {
-    event.currentTarget.className = ''
+    event.currentTarget.classList.remove(DRAG_OVER_CLASS_NAME)
   }
 
   function dragOver (event: React.DragEvent) {
     event.preventDefault()
-    event.currentTarget.className = 'dragover'
+    event.currentTarget.classList.add(DRAG_OVER_CLASS_NAME)
   }
 
   return (
@@ -39,6 +40,7 @@ export const DraggableList = (): JSX.Element => {
             </li>
           )
         })}
+        <li className='clear' onDrop={drop} onDragOver={dragOver} onDragLeave={dragLeave}></li>
       </StyledDraggableList>
     </>
   )
--- src/components/StyledDraggableList.tsx
+++ src/components/StyledDraggableList.tsx
@@ -2,8 +2,17 @@ import styled from '@emotion/styled'
 
 export const StyledDraggableList = styled.ul(
   {
+    '& li': {
+      width: '200px',
+      padding: '10px 15px',
+      borderRadius: '4px',
+      backgroundColor: '#ccc'
+    },
+    '& li.clear': {
+      opacity: 0
+    },
     '& li.dragover': {
-      borderTop: 'solid 2px #bababa'
+      borderTop: 'solid 3px #cdcdcd'
     }
   }
 )
